42bd34fcbc7e5a5d276d42fe46487228e7c696a9
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-09 20:23:15 -0400

    fetchDadJoke

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+const fecthDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "learning app",
+    },
+  });
+  const data = await response.json();
+  result.textContent = data.joke;
+};
+
+btn.addEventListener("click", fecthDadJoke);
+
+fecthDadJoke();
